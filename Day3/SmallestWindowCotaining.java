45a1534510c198ef91f2be41b4a8b615d9277755
nireeksh19 <niree999@gmail.com>
2024-01-03 10:11:29 +0530

    update jan 3

---
+++ Day3/SmallestWindowCotaining.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int smallestSubstring(String S) {
+        // Code here
+        int zero=-1,one=-1, two=-1,ans=Integer.MAX_VALUE;
+       
+       for(int i=0;i<S.length();i++){
+           if(S.charAt(i)=='0'){
+               zero=i;
+           }
+           else if(S.charAt(i)=='1'){
+
+               one=i;
+
+           }
+           else{
+               two=i;
+           } if(zero != -1 && one!= -1 && two != -1){
+                
+           ans=Math.min(ans,i+1-Math.min(zero,Math.min(one,two)));
+            }
+       }
+       if(ans==Integer.MAX_VALUE)
+       return -1;
+       else return ans;
+    }
+};
